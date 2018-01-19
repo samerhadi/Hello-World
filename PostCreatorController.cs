e2bee053ab86e9c39ebe0dc74587937d29b64af2
samerhadi <31986859+samerhadi@users.noreply.github.com>
2018-01-19 09:40:55 +0100

    Add files via upload

---
+++ PostCreatorController.cs
@@ -0,0 +1,33 @@
+﻿using Grupp38.Models;
+using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace Grupp38.API
+{
+    public class PostCreatorController : ApiController
+    {
+        //Get DBContext
+        protected ApplicationDbContext DB = new ApplicationDbContext();
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) DB.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        //Add row with postDetails to DB.Posts
+        [HttpPost]
+        public void UsePostCreator(Post postDetails)
+        {
+            var FromUser = DB.Users.Single(x => x.UserName == User.Identity.Name);
+            postDetails.From = FromUser;
+            var ToUser = DB.Users.Single(x => x.Id == postDetails.ToUser);
+            postDetails.To = ToUser;
+            postDetails.Text = postDetails.Text;
+            postDetails.Date = DateTime.Now;
+            DB.Posts.Add(postDetails);
+            DB.SaveChanges();
+        }
+    }
+}
